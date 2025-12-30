153dc356edba2eb684de5c5cc869e4032d4fca9e
Bao Nguyen <baogavip00@gmail.com>
2025-12-30 16:00:34 +0700

    fix: get slot by start date and end date

--- EduQuiz.Application/UseCases/ClassSlot/GetClassSlotsByStudentId/GetClassSlotsByStudentIdUseCaseValidator.cs
+++ EduQuiz.Application/UseCases/ClassSlot/GetClassSlotsByStudentId/GetClassSlotsByStudentIdUseCaseValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using FluentValidation;
 
 namespace EduQuiz.Application.UseCases.ClassSlot
 {
@@ -6,7 +7,45 @@ namespace EduQuiz.Application.UseCases.ClassSlot
     {
         public GetClassSlotsByStudentIdUseCaseValidator()
         {
+            RuleFor(x => x.Date)
+                .Custom((date, context) =>
+                {
+                    if (date != null && !DateTime.TryParse(date, out var datetime))
+                    {
+                        context.AddFailure("Date", "Date must be a date");
+                    }
+                });
 
+            RuleFor(x => x.StartDate)
+                .Custom((startDate, context) =>
+                {
+                    var instance = context.InstanceToValidate;
+
+                    if (startDate != null)
+                    {
+                        if (!DateTime.TryParse(startDate, out var startDatetime))
+                        {
+                            context.AddFailure("StartDate", "Start date must be a date");
+                        }
+
+                        if (DateTime.TryParse(instance.EndDate, out var endDatetime))
+                        {
+                            if (startDatetime > endDatetime)
+                            {
+                                context.AddFailure("StartDate", "Start date must be before end date.");
+                            }
+                        }
+                    }                   
+                });
+
+            RuleFor(x => x.EndDate)
+                .Custom((endDate, context) =>
+                {
+                    if (endDate != null && !DateTime.TryParse(endDate, out var endDatetime))
+                    {
+                        context.AddFailure("EndDate", "End date must be a date");
+                    }
+                });
         }
     }
 }
