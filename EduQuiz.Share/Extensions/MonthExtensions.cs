6380dc956010b6e8112a44ead032882b513ca675
Bao Nguyen <baogavip00@gmail.com>
2026-02-05 13:05:02 +0700

    fix: print academic report

--- EduQuiz.Share/Extensions/MonthExtensions.cs
+++ EduQuiz.Share/Extensions/MonthExtensions.cs
@@ -4,20 +4,21 @@
     {
         public static string ToVietnameseMonth(this string month)
         {
+            month = month.ToLower();
             return month switch
             {
-                "January" => "Tháng 1",
-                "February" => "Tháng 2",
-                "March" => "Tháng 3",
-                "April" => "Tháng 4",
+                "january" => "Tháng 1",
+                "february" => "Tháng 2",
+                "march" => "Tháng 3",
+                "april" => "Tháng 4",
                 "May" => "Tháng 5",
-                "June" => "Tháng 6",
-                "July" => "Tháng 7",
-                "August" => "Tháng 8",
-                "September" => "Tháng 9",
-                "October" => "Tháng 10",
-                "November" => "Tháng 11",
-                "December" => "Tháng 12",
+                "june" => "Tháng 6",
+                "july" => "Tháng 7",
+                "august" => "Tháng 8",
+                "september" => "Tháng 9",
+                "october" => "Tháng 10",
+                "november" => "Tháng 11",
+                "december" => "Tháng 12",
                 _ => month
             };
         }
